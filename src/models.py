38dd28bef0e2e303338ce2986a6923e110e5d0d0
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-01-02 22:08:17 +0000

    Code enhancement

--- src/models.py
+++ src/models.py
@@ -98,10 +98,10 @@ class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=False)
-    character_id = db.Column(db.Integer, db.ForeignKey("character.id"), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), nullable=False)
     user = db.relationship(User)
     planet = db.relationship(Planet)
-    character = db.relationship(Character)
+    people = db.relationship(People)
 
     def __repr__(self):
         return '<Favorite %r>' % self.id
@@ -111,7 +111,7 @@ class Favorite(db.Model):
             "id": self.id,
             "user_id": self.user_id,
             "planet_id": self.planet_id,
-            "character_id": self.character_id,
+            "people_id": self.people_id,
 
         }
 
